e80d7c0f4142213de4eb6e9c5030dc5c799a40e0
Lukas Frey <lukas.frey@cnlab.ch>
2017-01-17 22:29:57 +0100

    Solution with component

--- js/app.js
+++ js/app.js
@@ -2,7 +2,14 @@
     angular.module("NarrowItDownApp", [])
         .service("MenuSearchService", MenuSearchService)
         .controller("NarrowItDownController", NarrowItDownController)
-        .directive("foundItems", FoundItemsDirective)
+        .component("foundItems", {
+            templateUrl: "foundItems.html",
+            controller: FoundItemsComponentController,
+            bindings: {
+                list: "<",
+                onRemove: "&"
+            }
+        })
         .constant('MenuItemsUrl', "https://davids-restaurant.herokuapp.com/menu_items.json");
  
  
@@ -13,6 +20,11 @@
         ctrl.found = [];
 
         ctrl.onNarrow = function() {
+            if (ctrl.searchTerm === "") {
+                ctrl.found = [];
+                return;
+            }
+            
             var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
             promise.then(function (result) {
                 // process result and only keep items that match
@@ -44,36 +56,15 @@
             return $http({
                 url: MenuItemsUrl
             });
-            /*return promise.then(function (result) {
-                // process result and only keep items that match
-                var filtered = [];
-                var menuItems = result.data.menu_items;
-                for (index = 0; index < menuItems.length; ++index) {
-                    item = menuItems[index];
-                    if (item.description.indexOf(searchTerm) !== -1) {
-                        filtered.push(item);
-                    }
-                }
-                // return processed items
-                console.log(filtered);
-                
-                return filtered;
-            });*/
         };
     };
-    
-    function FoundItemsDirective() {
-        var ddo = {
-            templateUrl: 'foundItems.html',
-            scope: {
-                found: '<foundItems'
-                //onRemove: '&'
-            },
-            controller: NarrowItDownController,
-            controllerAs: 'list',
-            bindToController: true
-        };
 
-        return ddo;
-    }
+    FoundItemsComponentController.$inject = ["$element"];
+    function FoundItemsComponentController($element) {
+        var $ctrl = this;
+        
+        $ctrl.remove = function (myIndex) {
+            $ctrl.onRemove({ index: myIndex });
+        };
+    };
 })();
